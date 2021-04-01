82ae3fc0b74c8dc2241e3f9a058c9137df589729
Martin Pszczola <martinpsz@protonmail.com>
2021-04-01 16:49:22 -0700

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,15 +1,43 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+module.exports = ({ env }) => {
+  const parse = require('pg-connection-string').parse;
+
+  if (env('NODE_ENV') === 'production') {
+    const config = parse(process.env.DATABASE_URL)
+
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false
+          }
+        }
+      }
+    }
+  }
+  
+    return {
+      defaultConnection: 'default',
+        connections: {
+      default: {
+          connector: 'bookshelf',
+            settings: {
+            client: 'sqlite',
+              filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+          options: {
+            useNullAsDefault: true,
+        },
+        },
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+    }
+};
