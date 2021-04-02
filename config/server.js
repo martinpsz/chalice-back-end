da2c947425b7eb08bd28c0b5f96480e35c989083
Martin Pszczola <martinpsz@protonmail.com>
2021-04-01 20:16:57 -0700

    initial commit

--- config/server.js
+++ config/server.js
@@ -1,11 +1,10 @@
 module.exports = ({ env }) => ({
-   // host: env('HOST', '0.0.0.0'),
-   //   port: env.int('PORT', 1337),
-   //     admin: {
-   //   auth: {
-   //     secret: env('ADMIN_JWT_SECRET', 'f2b2b76f0e2907ae41aa3fdc6be04ce6'),
-   // },
-  //},
-  url: env('HEROKU_URL')
+    host: env('HOST', '0.0.0.0'),
+      port: env.int('PORT', 1337),
+        admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET', 'f2b2b76f0e2907ae41aa3fdc6be04ce6'),
+   },
+  }
     
 });
